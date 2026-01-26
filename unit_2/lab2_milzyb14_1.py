3fc70dcf1735ff435e3677a494b268b9c624a3ca
milzyb14 <buchananmyles1@gmail.com>
2026-01-25 20:40:26 -0500

    Added tip calulations as well as print statements that display the final total bill amount with the tip included.

--- unit_2/lab2_milzyb14_1.py
+++ unit_2/lab2_milzyb14_1.py
@@ -12,5 +12,13 @@ print(welcome_message)
 bill_total = float(input("Please enter the total bill amount: $"))
 tip_15 = bill_total * 0.15
 tip_20 = bill_total * 0.20
+print(f"A 15% tip would be: ${tip_15:.2f}")
+print(f"A 20% tip would be: ${tip_20:.2f}")
+
+final_total_15 = bill_total + tip_15
+final_total_20 = bill_total + tip_20
+print(f"The total bill including a 15% tip would be: ${final_total_15:.2f}")
+print(f"The total bill including a 20% tip would be: ${final_total_20:.2f}")    
+
 welcome_screen = f"{bill_total:.2f}"
 print(welcome_screen)
