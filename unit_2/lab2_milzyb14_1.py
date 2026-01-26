9f6e8dc58c177df58f3bb7a976007d53465e7880
milzyb14 <buchananmyles1@gmail.com>
2026-01-25 20:16:10 -0500

    Added a triple quoted comment with the name of the program, the author, the purpose, starter code, and the date

---
+++ unit_2/lab2_milzyb14_1.py
@@ -0,0 +1,7 @@
+''' Name: Tip Your Server or Bartender Calculator 
+    Author: Myles Buchanan 
+    Purpose: The purpose of this program is to calculate the tip amount at 15 and 20 percent, 
+    as well as the total bill including tip, based on user input of the bill amount.
+    Starter code: None
+    Date: September 12, 2023
+'''
